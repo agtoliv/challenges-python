b474bc9fad78e80c9998dcfe0693e2465554909c
agtoliv <augustooliveira.silveira@gmail.com>
2024-03-05 14:57:26 -0300

    feat: ends question 4

--- Q5.py
+++ Q5.py
@@ -8,3 +8,18 @@ a) Essa string pode ser informada através de qualquer entrada de sua preferênc
 
 b) Evite usar funções prontas, como, por exemplo, reverse;
 """
+
+def inverter_string(s):
+    string_invertida = ''  
+    for i in range(len(s) - 1, -1, -1):  
+        string_invertida += s[i]  
+    return string_invertida
+
+
+s = input("Digite uma string para ser invertida: ")
+
+
+string_invertida = inverter_string(s)
+
+
+print("String invertida:", string_invertida)
